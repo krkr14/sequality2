f04012b45f95f09784a9d8da1398fce3f51ea210
krkr14 <e1c19039@st.oit.ac.jp>
2022-07-04 16:59:51 +0900

    テスト項目のチューニング

--- app/src/test/java/sequality/CalculateTest.java
+++ app/src/test/java/sequality/CalculateTest.java
@@ -11,9 +11,11 @@ public class CalculateTest {
         assertEquals(expected, calculate.sum(2, 3));
         assertEquals(2.5, calculate.ave(2, 3), delta);
         assertEquals(55, calculate.sumArray(1, 10));
+        assertEquals(55, calculate.sumArray(10, 1));
         assertEquals(5.5, calculate.aveArray(1, 10), delta);
-        // assertEquals(5.5, calculate.aveArray(10, 1), delta);
+        assertEquals(5.5, calculate.aveArray(10, 1), delta);
         assertEquals(25, calculate.sumOdd(1, 10));
-        assertEquals(30, calculate.sumEven(1, 10));
+        assertEquals(1, calculate.sumOdd(1, 1));
+        assertEquals(0, calculate.sumEven(1, 1));
     }
 }
